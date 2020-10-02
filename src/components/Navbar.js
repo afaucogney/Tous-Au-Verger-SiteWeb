74c26cb2b21479893d4cbfd1c8aa572971d6aeed
Faucogney Anthony <anthony.faucogney@free.fr>
2020-10-02 18:55:55 +0200

    Update Navbar.js
    remove github link 
    translate to french
    remove products

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,10 +61,7 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+                A propos
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
@@ -72,21 +69,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
